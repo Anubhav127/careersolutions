a619b37b62117d362d6102220832f98eb2490212
Anubhav127 <anu.verma712@gmail.com>
2025-05-24 13:12:15 +0530

    Added herosection

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/HeroSection";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="grid-background" />
+      <HeroSection />
+    </div>
+  );
+}

83ccbe228c08607bd686c42701e3e9a353573d0d
Anubhav127 <anu.verma712@gmail.com>
2025-05-28 17:04:49 +0530

    footer updated

--- app/layout.js
+++ app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
 
               <footer className="bg-muted/50 py-12">
                 <div className="container mx-auto px-4 text-center text-gray-200">
-                  <p>Made by Anubhav</p>
+                  <p>© 2025 CareerCraft. All rights reserved.</p>
                 </div>
               </footer>
             </ThemeProvider>
